94178f2e3f8682a9133adc5c85277869dd92dbb5
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2021-07-29 12:20:19 +0530

    Create template.cpp
    My template with debugger , make an error.txt file and use pika(var_name) to debug the code . No need to comment out the keyword " pika "

---
+++ template.cpp
@@ -0,0 +1,78 @@
+/*    Author : Prakhar Rai    */
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define LB(x,num) lower_bound(x.begin(),x.end(),num) - x.begin()
+#define UB(x,num) upper_bound(x.begin(),x.end(),num) - x.begin()
+#define BS(x,num) binary_search(x.begin(),x.end(),num)
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define vci vector<int>
+#define vcll vector<ll>
+#define vcd vector<long double>
+#define line(x) sort(x.begin(),x.end())
+#define all(x) x.begin(),x.end()
+#define newl "\n"
+#define vc vector
+#define loop(i,a,b) for(int i = a; i < b; i++)
+#define lol(i,a,b) for(ll i = a; i < b; i++)
+#define lod(i,a,b) for(ld i = 0; i < b; i++)
+#define mod 1000000007
+#define read(v,n) lol(i,0,n) {ll x; cin >> x; v.pb(x);}
+#define run(a,x) for(auto x : a)
+#define yes "YES"
+#define no "NO"
+ll a, b, cnt;
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define pika(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define pika(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fs); cerr << ","; _print(p.sc); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+// == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == = main code == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+
+
+
+
+void solve() {
+
+
+
+}
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+}
